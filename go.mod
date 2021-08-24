b45fe41d68638936d7ff7490546c18995f994e75
周帅明 <smzhou@corp.netease.com>
2021-08-24 12:53:23 +0800

    http.ServerMux implement of pod.Handler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shuaiming/mux
+
+go 1.16
---
+++ mux.go
@@ -0,0 +1,20 @@
+package mux
+
+import "net/http"
+
+// Mux route http request
+type Mux struct {
+	http.ServeMux
+}
+
+// New HTTP Servermux
+func New(m http.ServeMux) *Mux {
+	return &Mux{m}
+}
+
+// ServeHTTP implement pod.Handler
+func (m *Mux) ServeHTTP(
+	rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	m.ServeMux.ServeHTTP(rw, r)
+	next(rw, r)
+}
